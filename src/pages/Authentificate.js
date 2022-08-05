02b1427184904a0be08f13ce785c1fd0e3f050b0
essadany <essadanyya@cy-tech.fr>
2022-08-05 09:02:11 +0200

    first commit

---
+++ src/pages/Authentificate.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import logo from '../assets/img/logo.png'
+import './Auth.css'
+const Authentificate = () => {
+  return (
+    <div>
+        <img className="mb-4" src={logo} alt="" width="500" height="200" />
+      <h1 className="h3 mb-3 fw-normal" >Gestion du Personnel</h1>
+      <form className="container col-3" method="post" action="" >
+          <div className="mb-3 ">
+            <label htmlFor="exampleInput" className="form-label">Identifiant :</label>
+            <input type="text" className="form-control" id="exampleInput" name="id" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Mot de passe :</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" name="pwd" />
+          </div>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Resté connecté?</label>
+          </div>
+          <input type="submit" className="btn btn-primary" name="valider" value="Connecter" />
+      </form>
+    </div>
+  )
+}
+export default Authentificate;
