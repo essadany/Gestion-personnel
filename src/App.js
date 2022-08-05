ef00e5a0a19777197f281369ee9ec08de0a994c7
essadany <essadanyya@cy-tech.fr>
2022-08-05 14:10:15 +0200

    add cards exemples in accueil.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import Authentificate from './pages/Authentificate';
 import Profile from './pages/Profile';
+import Accueil from './pages/Accueil';
 
 function App() {
   return (
-    <Profile />
+    <Accueil />
   );
 }
 
